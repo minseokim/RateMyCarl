7249f975f280d2265d5f7e45e97235d1c860db9d
Alex Kim <minseoalexkim@gmail.com>
2016-12-13 10:38:37 +0900

    feat: get results from ratemyprofessors and find professor profile page

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 const formatProfNameWithSpace = (name) => {
 
   let nameArr = name.split(" ");
-  let formattedProfName = nameArr[0] + " " + nameArr[nameArr.length-1];
+  let formattedProfName = nameArr[0] + "+" + nameArr[nameArr.length-1];
 
   return formattedProfName;
 };
@@ -17,9 +17,7 @@ const formatProfNameWithSpace = (name) => {
 /*
   Fetches all professor names from enroll page and stores in a set
 */
-const getNamesFromEnroll = function() {
-
-  let profNameSet = new Set();
+const getNamesFromEnroll = () => {
 
   let profNamesFromEnroll = document.getElementsByClassName("faculty");
 
@@ -32,24 +30,18 @@ const getNamesFromEnroll = function() {
     let profNames = facultyNode.innerText.split(", ");
 
     for (let i = 0; i < profNames.length; i++) {
-      profNameSet.add(formatProfNameWithSpace(profNames[i]));
+      let finalizedProfName = formatProfNameWithSpace(profNames[i]);
+      findProfessorPage(finalizedProfName);
     }
 
   });
-
-  // profNameSet.forEach((name) => {
-  //   console.log(name);
-  // });
-
-  return profNameSet;
 };
+
 
 /*
   Fetches all professor names from The Hub and stores them in a set
 */
-const getNamesFromHub = function() {
-
-  let profNameSet = new Set();
+const getNamesFromHub = () => {
 
   let profNamesFromHub = document.getElementsByClassName(" SEC_FACULTY_INFO");
 
@@ -66,29 +58,88 @@ const getNamesFromHub = function() {
     let profNames = facultyNode.innerText.replace(/,|\n/g , "").split(" ");
 
     for (let i = 0; i < profNames.length; i+=2) {
-      let formattedProfName = profNames[i+1] + " " + profNames[i];
-      console.log(formattedProfName);
-      profNameSet.add(formattedProfName);
+      let formattedProfName = profNames[i+1] + "+" + profNames[i];
+      //get ratings from findProfessorPage
+      findProfessorPage(formattedProfName);
+      //
     }
   });
 
   return profNameSet;
-
 };
 
 
-const addRatings = function() {
-// http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=Carleton+College&queryoption=HEADER&query=Kent+Freeze&facetSearch=true
+/*
+  Finds professor's specific profile page from initial search results
+*/
+const findProfessorPage = (professorName) => {
 
-  // console.log(window.location);
-  // hostname : apps.carleton.edu
-  // pathname : /campus/registrar/schedule/enroll/
+  //Our url for making initial request to find professor's profile page
+  let url = "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=Carleton+College&queryoption=HEADER&query=PROFESSORNAME&facetSearch=true";
+  url = url.replace("PROFESSORNAME", professorName);
+
+  let tempDiv = document.createElement('div');
+
+  let requestInfo = {
+    method: 'GET',
+    url: url,
+  };
+
+  chrome.runtime.sendMessage(requestInfo, function(data) {
+
+    tempDiv.innerHTML = data;
+    let foundProfessors = tempDiv.getElementsByClassName('listing PROFESSOR');
+    // console.log(foundProfessors);
+
+    if (foundProfessors.length === 0) {
+      //No search results, populate result UI with "N/A"
+    } else {
+      // //Iterate through search results and verify by comparing first name and last name
+      // for (let i = 0; i < foundProfessors.length; i++) {
+      //   let currentProfessor = foundProfessors[i];
+
+      //   let foundName = currentProfessor.getElementsByClassName('main')[0].innerText; // returns "Freeze, Kent "
+      //   foundName = foundName.replace(/,|\n/g , "").trim().split(" ").reverse();
+      //   professorNameArray = professorName.split(" ");
+      //   //if name isn't matching, break and return;
+      //   if (foundName[0] !== professorNameArray[0] || foundName[1] !== professorNameArray[1]) {
+      //     break;
+      //   }
+      // }
+
+      //There is a search result, fetch professor's profile page link
+        let link = foundProfessors[0].getElementsByTagName("a")[0].getAttribute("href"); ///ShowRatings.jsp?tid=2108435
+        let profilePageLink = "http://www.ratemyprofessors.com" + link;
+        console.log(profilePageLink);
+
+        //make second request
+        chrome.runtime.sendMessage({
+          method: 'GET',
+          url: profilePageLink
+        }, function(response) {
+          // addRatingsToPage(response);
+          console.log('ratings successfully received');
+        });
+    }
+
+  });
+
+};
+
+const main = () => {
 
   //check which site we're on, and call handleEnrollPage or handleHubPage
+  if (window.location.hostname === "apps.carleton.edu") {
+    getNamesFromEnroll();
+  } else if (window.location.hostname === "thehub.carleton.edu") {
+    getNamesFromHub();
+  }
 };
 
 
-//fetches ratings from RateMyProfessor.com
-const fetchRatings = function() {
+const addRatingsToPage = () => {
+
 
 };
+
+main();
--- src/bg/background.js
+++ src/bg/background.js
@@ -1,4 +1,25 @@
+function showPageAction(tabId, changeInfo, tab) {
+  if (tab.url.hostname === "apps.carleton.edu" || tab.url.hostname === "thehub.carleton.edu") {
+    chrome.pageAction.show(tabId);
+  }
+}
+
+chrome.tabs.onUpdated.addListener(showPageAction);
+
 //function for making requests to RateMyProfessor
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
   let xhr = new XMLHttpRequest();
+
+  xhr.onload = function() {
+    callback(xhr.responseText);
+  }
+
+  xhr.onerror = function() {
+    callback();
+  }
+
+  xhr.open(request.method, request.url, true);
+  xhr.send();
+
+  return true;
 });
